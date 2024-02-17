469e3fff8c8b67dd350308b3ba1e90eb308a818d
stefan <stefanbvts@gmail.com>
2024-02-17 01:04:53 +0100

    pocetna stranica

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>SkillUp | 2024</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./style/indexStyle.css">
+</head>
+
+<body></body>
+<header>
+  <div class="upper-header">
+    <div class="logo">
+      <img src="./assets/site_pics/logo.svg" alt="languages" width="38" height="auto">
+    </div>
+    <div class="languages">
+      <img src="./assets/site_pics/lang.svg" alt="languages" width="28" height="auto">
+    </div>
+  </div>
+
+  <div class="nav-header">
+    <a href="#">Naslovna</a>
+    <a href="#">O nama</a>
+    <a href="#">Projekti</a>
+    <a href="#">Blog</a>
+    <a href="#">Kontakt</a>
+  </div>
+</header>
+
+
+
+<div class="content-wrapper">
+  <div class="hero">
+    <div class="hero_text">
+      <h2>Zajedno za Zemlju: Ekološka platforma za inspiraciju i akciju</h2>
+      <p>Povežite se, inspirišite se, delujte zajedno za našu planetu! Dobrodošli na našu platformu posvećenu ekologiji i razmeni ideja za održivu budućnost.</p>
+    </div>
+    <a class="dugme_hero" href="">Pročitaj više</a>
+  </div>
+  <div class="wrapper">
+    <h2>Projekti</h2>
+    <div class="cols">
+      <div class="col" ontouchstart="this.classList.toggle('hover');">
+        <div class="container">
+          <div class="front" style="background-image: url(https://unsplash.it/500/500/)">
+            <div class="inner">
+              <p>Diligord</p>
+              <span>Lorem ipsum</span>
+            </div>
+          </div>
+          <div class="back">
+            <div class="inner">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="container">
+          <div class="front" style="background-image: url(https://unsplash.it/511/511/)">
+            <div class="inner">
+              <p>Rocogged</p>
+              <span>Lorem ipsum</span>
+            </div>
+          </div>
+          <div class="back">
+            <div class="inner">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col" ontouchstart="this.classList.toggle('hover');">
+        <div class="container">
+          <div class="front" style="background-image: url(https://unsplash.it/502/502/)">
+            <div class="inner">
+              <p>Strizzes</p>
+              <span>Lorem ipsum</span>
+            </div>
+          </div>
+          <div class="back">
+            <div class="inner">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="container">
+          <div class="front" style="background-image: url(https://unsplash.it/503/503/)">
+            <div class="inner">
+              <p>Clossyo</p>
+              <span>Lorem ipsum</span>
+            </div>
+          </div>
+          <div class="back">
+            <div class="inner">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="onama">
+    <img src="./assets/site_pics/oNama.png" alt="onama-hero">
+    <div class="onama_desno">
+      <div class="text_onama">
+        <h3>O nama:</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not o</p>
+      </div>
+      <div class="text_onama_innerInfo">
+        <div class="team3">
+          <p class="Ime_tima">Team3</p>
+          <p class="SDG">Cilj odživog razvoja 12</p>
+        </div>
+        <a href="#">Pretraži više</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="wrapper">
+    <h2>Blog</h2>
+    <div class="cols">
+      <div class="col" ontouchstart="this.classList.toggle('hover');">
+        <div class="container">
+          <div class="front" style="background-image: url(https://unsplash.it/500/500/)">
+            <div class="inner">
+              <p>Diligord</p>
+              <span>Lorem ipsum</span>
+            </div>
+          </div>
+          <div class="back">
+            <div class="inner">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="container">
+          <div class="front" style="background-image: url(https://unsplash.it/511/511/)">
+            <div class="inner">
+              <p>Rocogged</p>
+              <span>Lorem ipsum</span>
+            </div>
+          </div>
+          <div class="back">
+            <div class="inner">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col" ontouchstart="this.classList.toggle('hover');">
+        <div class="container">
+          <div class="front" style="background-image: url(https://unsplash.it/502/502/)">
+            <div class="inner">
+              <p>Strizzes</p>
+              <span>Lorem ipsum</span>
+            </div>
+          </div>
+          <div class="back">
+            <div class="inner">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="container">
+          <div class="front" style="background-image: url(https://unsplash.it/503/503/)">
+            <div class="inner">
+              <p>Clossyo</p>
+              <span>Lorem ipsum</span>
+            </div>
+          </div>
+          <div class="back">
+            <div class="inner">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="carouselExampleIndicators" class="carousel carousel-dark slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <h2>Komentari</h2>
+        <p>Oduševljena sam što sam pronašla ovu platformu! Konačno mogu da podelim svoje ideje za održivu budućnost i da se povežem sa ljudima koji dele istu strast. Hvala vam što ste omogućili ovu razmenu znanja i inspiracije za bolji svet!</p>
+        <div class="nalog">
+          <div class="text_nalog">
+            <p class="nalog_ime">Marija Jovanović</p>
+            <p class="nalog_lokacija">Beograd</p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sapiente reprehenderit hic. Quis eligendi nesciunt quibusdam. Unde nihil ipsa nostrum, ab quidem vitae voluptate repellat recusandae. Cum natus quae porro.</span><span>Enim autem praesentium eos expedita in provident modi, reiciendis non quibusdam voluptate placeat soluta ut facere eligendi est eveniet quis cupiditate fuga laudantium necessitatibus earum. Quis eaque voluptate assumenda neque?</span></p>
+      </div>
+      <div class="carousel-item">
+        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sapiente reprehenderit hic. Quis eligendi nesciunt quibusdam. Unde nihil ipsa nostrum, ab quidem vitae voluptate repellat recusandae. Cum natus quae porro.</span><span>Enim autem praesentium eos expedita in provident modi, reiciendis non quibusdam voluptate placeat soluta ut facere eligendi est eveniet quis cupiditate fuga laudantium necessitatibus earum. Quis eaque voluptate assumenda neque?</span></p>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
+
+<footer>
+  <img class="slika" src="assets/site_pics/logo.svg" alt="">
+  <div class="footerdole">
+    <p>Copyright@Team3</p>
+
+    <div class="stranicefooter">
+      <a href="#">Naslovna</a>
+      <a href="#">O nama</a>
+      <a href="#">Projekti</a>
+      <a href="#">Blog</a>
+      <a href="#">Kontakt</a>
+    </div>
+  </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
