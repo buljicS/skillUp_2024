40a91fb8c3e87b651bc148ed1edbe5623800257c
stefan <stefanbvts@gmail.com>
2024-02-26 23:34:05 +0100

    promene

---
+++ projekti.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Projeti</title>
+</head>
+
+<body>
+    <header>
+        <div class="upper-header">
+            <div class="logo">
+                <img src="img/Logo.png" alt="languages" width="38" height="auto">
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <g clip-path="url(#clip0_259_630)">
+                    <path d="M12 23C18.075 23 23 18.075 23 12C23 5.925 18.075 1 12 1M12 23C5.925 23 1 18.075 1 12C1 5.925 5.925 1 12 1M12 23C15 23 16 18 16 12C16 6 15 1 12 1M12 23C9 23 8 18 8 12C8 6 9 1 12 1M2 16H22M2 8H22" stroke="#3A4D39" stroke-width="2" />
+                </g>
+                <defs>
+                    <clipPath id="clip0_259_630">
+                        <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                </defs>
+            </svg>
+        </div>
+
+        <div class="nav-header">
+            <a href="#">Naslovna</a>
+            <a href="#">O nama</a>
+            <a href="#">Projekti</a>
+            <a href="#">Blog</a>
+            <a href="#">Kontakt</a>
+        </div>
+    </header>
+
+    <div id="parallax-world-of-ugg">
+        <section>
+            <div class="parallax-three">
+                <h2>PROJEKTI</h2>
+            </div>
+        </section>
+
+        <section>
+            <div class="block">
+                <p><span class="first-character sc">D</span>obrodošli na stranicu posvećenu projektima koji su ključni za našu misiju zaštite okoline, poboljšanja kvaliteta vazduha i pošumljavanja. Kroz ove projekte nastojimo ostvariti pozitivan uticaj na našu zajednicu i svet oko nas. Projekti koje radimo jesu: Odgovorna potrošnja, Reciklaža i minimiziranje otpada (doniranje odeće; spajanje donora s primaocima), Usklađivanje kupaca i prodavaca, kao racionalna potrošnja, Zaštita vazduha
+                </p>
+                <p class="line-break margin-top-10"></p>
+                <p class="margin-top-10">Odgovorna potrošnja podrazumijeva svesno biranje proizvoda i usluga koji su ekološki prihvatljivi i proizvedeni uz poštovanje ljudskih prava.Praćenje svog budžeta i troškova pomaže nam da bolje upravljamo svojim finansijama i izbegnemo nepotrebno rasipanje novca.Recikliranje i ponovna upotreba predmeta su važni koraci u smanjenju otpada i očuvanju resursa našeg planeta.Informiranje se o etičkim praksama kompanija od kojih kupujemo može pomoći u podršci poslovanja koja promiču društvenu odgovornost i transparentnost.Razmišljanje o dugoročnim posljedicama naših potrošačkih navika pomaže nam stvoriti održiviji i prosperitetniji svijet za buduće generacije.</p>
+                <p class="line-break margin-top-10"></p>
+                <p class="margin-top-10">Reciklaža i minimiziranje otpada ključni su koraci u očuvanju okoliša. Doniranje odeće omogućava ponovno korištenje i produženje životnog veka odevnih predmeta, smanjujući potrebu za proizvodnjom novih. Inicijative koje povezuju donore s primaocima poput platformi za razmenu ili dobrotvornih organizacija olakšavaju proces doniranja i doprinose društvenoj solidarnosti. Kroz ovakve prakse, možemo smanjiti količinu otpada koju proizvodimo i podržati zajednice u potrebi, gradeći održiviji i humaniji svet.</p>
+                <p class="line-break margin-top-10"></p>
+                <p class="margin-top-10">Usklađivanje kupaca i prodavaca će doprineti racionalnoj potrošnji putem efikasnije distribucije resursa. Platforme koje omogućuju direktnu interakciju između kupaca i prodavaca, poput online tržišta ili aplikacija za deljenje resursa, olakšavaju pronalaženje proizvoda ili usluga koje su potrebne. Ovaj pristup može smanjiti nepotrebne troškove i resurse koji se koriste za posredničke procese, te poticati transparentnost i konkurenciju. Kroz ovakve inovacije, potrošači imaju veću kontrolu nad svojim izborima, dok se prodavateljima pružaju prilike za direktnu interakciju s tržištem, što može rezultirati efikasnijim i ekonomičnijim poslovanjem.</p>
+                <p class="line-break margin-top-10"></p>
+                <p class="margin-top-10">Zaštita vazduha je ključna za očuvanje zdravlja ljudi i ekosistema. Smanjenje emisija štetnih gasova iz industrije, transporta i drugih izvora ključno je za smanjenje zagađenja vazduha. Promoviranje korištenja obnovljivih izvora energije, kao što su solarna i vetrena energija, te podrška energetskoj efikasnosti u svim sektorima igraju važnu ulogu u smanjenju emisija i poboljšanju kvaliteta zraka. Edukacija o važnosti čistog vazduha i podizanje svesti o merama koje pojedinci i društvo mogu poduzeti ključni su koraci ka održavanju zdravog okruženja za buduće generacije.</p>
+            </div>
+        </section>
+    </div>
+
+    <div class="wrapper">
+        <h1>ISTAKNUTE PRIČE</h1>
+        <div class="cols">
+            <div class="col" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/500/)">
+                        <div class="inner">
+                            <p>Ime priče</p>
+                            <span>Lorem ipsum</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/500/500/)">
+                        <div class="inner">
+                            <p>Ime priče</p>
+                            <span>Lorem ipsum</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/502/502/)">
+                        <div class="inner">
+                            <p>Ime priče</p>
+                            <span>Lorem ipsum</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col" ontouchstart="this.classList.toggle('hover');">
+                <div class="container">
+                    <div class="front" style="background-image: url(https://unsplash.it/503/503/)">
+                        <div class="inner">
+                            <p>Ime priče</p>
+                            <span>Lorem ipsum</span>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inner">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit quae suscipit c.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="objava">
+        <img src="https://img.freepik.com/free-photo/business-people-brainstorming-ideas-plan-work-report-with-data-charts-graphs-analyzing-research-information-create-successful-project-office-partnership-collaboration_482257-47596.jpg?t=st=1708715218~exp=1708718818~hmac=309164da3c21373a68e1b435bf7bae505ff790a335c9e1e1d27f3b79f446b154&w=1380" alt="">
+        <div class="objava_text">
+            <div class="objava_info">
+                <h1>Podelite svoje ideje za ekološki projekat s nama</h1>
+                <p>Ako imate ideju za ekološki projekat, pozivamo vas da je podelite s nama! Verujemo da zajedničkim snagama možemo napraviti veliku razliku u očuvanju naše planete. Vaša ideja može biti ključna za pokretanje inspirativnog projekta koji će pozitivno uticati na životnu sredinu. Vaš predlog je dobrodošao i cenjen, jer svaki korak ka održivijoj budućnosti je važan.</p>
+            </div>
+            <a href="">Pređi na kontakt stranicu</a>
+        </div>
+    </div>
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.1/vue.min.js'></script>
+    <script src="./script.js"></script>
+</body>
+
+</html>
